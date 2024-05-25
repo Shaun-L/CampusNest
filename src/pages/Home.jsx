4c50d086b7b0e9c627a226953c01952cd3697968
Philip Truong <philtr928@gmail.com>
2024-05-24 22:38:35 -0700

    move map

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,62 +1,9 @@
-import { useState } from "react";
-import { MapContainer, TileLayer, useMapEvents, Marker, MapPlaceholder, Popup } from "react-leaflet";
 
-import './Home.css';
-
-// const LocationMarker = () => {
-//   const [position, setPosition] = useState(null);
-//   const map = useMapEvents({
-//     click() {
-//       map.locate();
-//     },
-//     locationfound(e) {
-//       setPosition(e.latlng);
-//       map.flyTo(e.latlng, map.getZoom());
-//     },
-//   });
-
-//   return position === null ? null : (
-//     <Marker position={position}>
-//       <Popup>You are here</Popup>
-//     </Marker>
-//   );
-// }
 
 const Home = () => {
-  const [center, setCenter] = useState([51.0, 19.0]);
-  
-  return (
-    <div className="w-100 h-full rounded">
-      <h1>bladhfhapd</h1>
-      {/* <MapContainer
-    center={{ lat: 51.505, lng: -0.09 }}
-    zoom={13}
-    scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <LocationMarker />
-  </MapContainer> */}
-  <MapContainer
-      className="markercluster-map"
-      center={center}
-      zoom={4}
-      maxZoom={18}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-
-      {/* <MarkerClusterGroup>
-        <Marker position={[49.8397, 24.0297]} />
-        <Marker position={[52.2297, 21.0122]} />
-        <Marker position={[51.5074, -0.0901]} />
-      </MarkerClusterGroup> */}
-    </MapContainer>
-    </div>
-  );
+  return <div>
+    
+  </div>;
 };
 
 export default Home;
