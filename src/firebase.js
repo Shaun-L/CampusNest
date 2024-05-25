924d9cf1d5934f0e3b78cb247d9ebbe527949ebf
Elise Ji <ccelisej.04@gmail.com>
2024-05-24 22:10:01 -0700

    connected database

---
+++ src/firebase.js
@@ -0,0 +1,32 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA4jQ3Ts7160PqHX3xy-UOHxAWwb0Lm5eo",
+  authDomain: "venushacks2024-82733.firebaseapp.com",
+  projectId: "venushacks2024-82733",
+  storageBucket: "venushacks2024-82733.appspot.com",
+  messagingSenderId: "857445499678",
+  appId: "1:857445499678:web:ced8205d8a1ef9a7c83f0f",
+  measurementId: "G-LVBJFDE7KS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+
+var citiesRef = db.collection("testing");
+// curse_needed.value()
+
+
+
+console.log("Document written with ID: ", citiesRef);
