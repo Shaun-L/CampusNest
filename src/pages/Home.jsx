0be9e6c7ea982d2daea37a9c5022a37144e3047d
Elise Ji <ccelisej.04@gmail.com>
2024-05-25 15:52:26 -0700

    outlined listings page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,8 @@ import pic4 from "./pictures/pic4.jpg";
 import infop1 from "./pictures/infop1.png"
 import infop2 from "./pictures/infop2.jpg"
 import SearchBar from "../components/SearchBar"
-import LOGO from "./pictures/LOGO.png"
 import { useEffect, useNavigate } from "react";
+import LOGO1 from "./pictures/LOGO1.png"
 
 
 import './Home.css';
@@ -33,7 +33,7 @@ const Home = () => {
       <div className="top-halfp1">
 
         <div className='format-logo'>
-            <img src={LOGO} alt="lite" className="logo" />
+            <img src={LOGO1} alt="lite" className="logo" />
         </div>
 
         <div className="main-search">
@@ -56,7 +56,7 @@ const Home = () => {
 
 
           <div className='format-pics'>
-            <a href="/listing.jsx">
+            <a href="/listing/1">
               <img src={pic2} alt="lite" className="butterfly" />
             </a>
           </div>
--- src/pages/listing.jsx
+++ src/pages/listing.jsx
@@ -1,1 +1,72 @@
-<h2>HIIII</h2>
+import { useState } from "react";
+import pic2 from "./pictures/pic2.jpg";
+
+
+import './Listing.css';
+
+const Listing = () => {
+
+
+
+return (
+
+    <div className="container">
+
+
+
+        <div className="cont1">
+
+            <div className="picture">
+                <img src={pic2} alt="lite" className="butterfly" />
+            </div>
+
+            <div className="stats">
+                <h2 className="cost">$900/Month</h2>
+                <h2 className="address">15000 Arroyo Dr, Irvine, CA 92697</h2>
+                <h2 className="room">Plaza Verde, 2 Bed 2 Bath - Floor Plan A</h2>
+                {/* <h2 className="contacts">Phone#: 714-XXX-XXXX</h2> */}
+            </div>
+
+        </div>
+
+
+
+
+        <div className="more-details">
+            
+
+            <h2 className="deats-sub">
+                <span className="apartment-name">Female</span> Gender Preference
+            </h2>
+
+            <h2 className="deats-sub">
+                <span className="apartment-name">June 2024 - September 2024</span>  Duration
+            </h2>
+
+            <h2 className="deats-sub">
+                <span className="apartment-name">4 mi</span> Distance to School
+            </h2>
+
+            <h2 className="deats-sub">
+                <span className="apartment-name">Is</span> Walking Distance, Furnished
+            </h2>
+
+            <h2 className="small">Utilities include internet, gas, water, trash, and parking is available for purchase</h2>
+        
+            <h2 className="small">Contact elise@gmail.com, 714-XXX-XXXX for more details</h2>
+
+        
+
+        </div>
+    
+
+    </div>
+
+
+);
+
+
+
+
+}
+export default Listing;
