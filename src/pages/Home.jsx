940e7329b75b5ae1ffe897ba1371303b2f07cf63
Elise Ji <ccelisej.04@gmail.com>
2024-05-26 05:56:56 -0700

    pic updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import pic1 from "../assets/pic2.jpg";
+import pic1 from "../assets/pic1.jpg";
 import pic2 from "../assets/pic2.jpg";
 import pic3 from "../assets/pic3.jpg";
 import pic4 from "../assets/pic4.jpg";
